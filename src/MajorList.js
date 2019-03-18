4be4479bf9bf395a19988b5e998e57334debd4a7
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2019-03-18 15:28:52 -0400

    Updated Major List Class

---
+++ src/MajorList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class MajorList extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
