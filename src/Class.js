75eae0cb0a28eddb5efa6d64a467ba8b8bab8bc7
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2019-03-18 09:03:37 -0400

    Created a list of theoretical classes

---
+++ src/Class.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+class Class extends Component {
+    render() {
+        return(
+            <div>
+            <h3 className = 'Class'
+            style= {styles.Class}>MA111</h3>
+            </div>
+        )
+    }
+}
+
+const styles = {
+    Class: {
+        position: 'relative',
+        color: 'black',
+    }
+}
+
+export default Class;
--- src/ClassList.js
+++ src/ClassList.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import Class from './Class'
 
 const ClassList = () => {
     return(
         <div className = 'Files'
         style = {styles.Files}>
-            <h2>
-            Classes
-                
-            </h2>
-            
+            <h1>This is the beginning of classes</h1>
+           <ul className = 'List'
+           style = {styles.List}>
+               <li><Class></Class></li>
+               <li><Class></Class></li>
+            </ul>
             </div>
     )
 }
 const styles = {
     Files: {
+        display: 'flex',
         backgroundColor: 'grey',
         flex: 1,
         paddingBottom: '1rem',
         overflowY: 'scroll',
+    },
+    List: {
+        listStyle: 'none'
     }
 }
 
